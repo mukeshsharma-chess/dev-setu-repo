78d222d8499153e80273092dd1db75cfbbf16c82
Mukesh Sharma <sharmamks1992@gmail.com>
2025-10-06 19:29:37 +0530

    added layout

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Main Site",
+  description: "My App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
