1efd47ae4b54fe5532eae30a9547a0d2e7af9579
Mukesh Sharma <sharmamks1992@gmail.com>
2025-10-26 00:18:24 +0530

    added config file changed prifix

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,12 @@
 // next.config.mjs
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // i18n: {
-//   //   locales: ['en', 'hi'], // all supported languages
-//   //   defaultLocale: 'en',   // default language
-//   //   localeDetection: true,  // optional, stops auto-detection
-//   // },
-// };
-
-// export default nextConfig;  
-
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  async redirects() {
-    return [
-      // ✅ Remove locale prefixes (en/in) from API routes
-      {
-        source: '/:locale(en|in)/api/:path*',
-        destination: '/api/:path*',
-        permanent: true,
-      },
-
-      {
-        source: '/:locale(en|in)/:path*',
-        destination: '/:path*',
-        permanent: true,
-      },
-
-    ];
-  },
-
-  reactStrictMode: true,
+  // i18n: {
+  //   locales: ['en', 'hi'], // all supported languages
+  //   defaultLocale: 'en',   // default language
+  //   localeDetection: true,  // optional, stops auto-detection
+  // },
 };
 
-
-
-export default nextConfig; 
-
+export default nextConfig;
