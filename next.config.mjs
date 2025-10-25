2e253c10e31be9f12ffd542dd8dd1cb9007a7085
Mukesh Sharma <sharmamks1992@gmail.com>
2025-10-25 23:46:20 +0530

    added config file removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,25 @@
 // next.config.mjs
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // i18n: {
-//   //   locales: ['en', 'hi'], // all supported languages
-//   //   defaultLocale: 'en',   // default language
-//   //   localeDetection: true,  // optional, stops auto-detection
-//   // },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'in'],
-    defaultLocale: 'en',
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: '/:locale(en|in)/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
-  },
+  // i18n: {
+  //   locales: ['en', 'hi'], // all supported languages
+  //   defaultLocale: 'en',   // default language
+  //   localeDetection: true,  // optional, stops auto-detection
+  // },
 };
 
-
-
 export default nextConfig;
+
+
+
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:locale(en|in)/api/:path*',
+  //       destination: '/api/:path*',
+  //     },
+  //   ];
+  // }
+
